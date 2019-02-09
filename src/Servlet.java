028d7d71331cf60db24d57ff5e2f0d6ce35035e6
AurelienAubry <aurelien.aubry.dev@gmail.com>
2019-02-09 21:58:04 +0100

    Simple HTML page

---
+++ src/Servlet.java
@@ -0,0 +1,31 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		Date date = new Date();
+		out.println("<html><body>");
+		out.println("<h2>Welcome !</h2><br/><h3>Date="+date +"</h3>");
+		out.println("</body></html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
